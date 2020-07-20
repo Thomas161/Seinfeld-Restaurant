86a0234e762cfec6a7c34583448318f8c94696be
Tom Magin <magintc@mac.com>
2020-07-20 12:34:02 +1000

    added some animations

--- src/js/master.js
+++ src/js/master.js
@@ -1,100 +1,125 @@
-//  if(document.addEventListener){
+document.addEventListener("DOMContentLoaded", function (event) {
+  var count = 0;
+  var tl = new TimelineMax();
+  var cont, cont2, cont3;
+  cont = document.querySelector(".container");
+  cont2 = document.querySelector(".container2");
+  cont3 = document.querySelector(".container3");
+  tl.fromTo(cont, 0.9, { y: -400 }, { y: 0, ease: Power2.easeOut });
+  tl.fromTo(
+    cont2,
+    0.9,
+    { y: 400 },
+    { y: 0, ease: Power2.easeOut, delay: -0.3 }
+  );
 
-//  }
+  tl.fromTo(
+    cont3,
+    0.9,
+    { x: -400 },
+    { x: 0, ease: Power2.easeOut, delay: -0.3 }
+  );
 
-//DATABASE CONFIG AND METHODS
+  event.preventDefault();
 
-var config = {
-  apiKey: "AIzaSyDeDgV6YIjgWecHUTwNFP3nu6oPw899gls",
-  authDomain: "master-project-fd122.firebaseapp.com",
-  databaseURL: "https://master-project-fd122.firebaseio.com",
-  projectId: "master-project-fd122",
-  storageBucket: "",
-  messagingSenderId: "462770541013",
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-var ref = database.ref("Info"); //info is the node, traversing tree
-ref.on("value", gotData, errData);
-
-function display() {
-  var name = $("#patron").val();
-  console.log(name);
-  var time = $("#datetime24").val();
-  console.log(time);
-
-  var newPosts = {
-    person: name,
-    reservation: time,
+  checkLoop = () => {
+    if (count > 0) tl.stop();
+    else count++;
   };
-  ref.push(newPosts);
-}
+  //DATABASE CONFIG AND METHODS
 
-ref.on("value", function (snapshot) {
-  console.log(snapshot.key);
-  console.log(snapshot.val());
-});
+  var config = {
+    apiKey: "AIzaSyDeDgV6YIjgWecHUTwNFP3nu6oPw899gls",
+    authDomain: "master-project-fd122.firebaseapp.com",
+    databaseURL: "https://master-project-fd122.firebaseio.com",
+    projectId: "master-project-fd122",
+    storageBucket: "",
+    messagingSenderId: "462770541013",
+  };
+  firebase.initializeApp(config);
 
-function checker() {
-  let a = document.getElementById("patron").value;
-  let b = document.getElementById("datetime24").value;
+  var database = firebase.database();
+  var ref = database.ref("Info"); //info is the node, traversing tree
+  ref.on("value", gotData, errData);
 
-  if (
-    a == "" &&
-    a == null &&
-    a == undefined &&
-    b == "" &&
-    b == null &&
-    b == undefined
-  ) {
-    try {
-      throw "error";
-    } catch (err) {
-      return err;
-    }
-  } else return display();
-}
+  function display() {
+    var name = $("#patron").val();
+    console.log(name);
+    var time = $("#datetime24").val();
+    console.log(time);
 
-var person, reservation;
-
-function gotData(data) {
-  var id = data.val();
-  var keys = Object.keys(id);
-
-  console.log(keys);
-  for (var propName in keys) {
-    var k = keys[propName];
-    var person = id[k].person;
-    var reservation = id[k].reservation;
-
-    console.log(person);
-    var ul = document.getElementById("lister");
-    var li = document.createElement("li");
-
-    console.log(id);
-    li.appendChild(document.createTextNode(person));
-    li.appendChild(document.createTextNode(reservation));
-
-    // li.class("document");
-    ul.appendChild(li);
-    // li.class('cleanse');
+    var newPosts = {
+      person: name,
+      reservation: time,
+    };
+    ref.push(newPosts);
   }
-}
 
-function errData(err) {
-  console.log("Error");
-  console.log(err);
-}
+  ref.on("value", function (snapshot) {
+    console.log(snapshot.key);
+    console.log(snapshot.val());
+  });
 
-//VALIDATION METHODS FOR USER INPUT
+  function checker() {
+    let a = document.getElementById("patron").value;
+    let b = document.getElementById("datetime24").value;
 
-function myValidate(input) {
-  var vz = /[^a-z]/gi;
-  input.value = input.value.replace(vz, "");
-}
+    if (
+      a == "" &&
+      a == null &&
+      a == undefined &&
+      b == "" &&
+      b == null &&
+      b == undefined
+    ) {
+      try {
+        throw "error";
+      } catch (err) {
+        return err;
+      }
+    } else return display();
+  }
 
-function complete() {
-  document.getElementById("patron").value = "";
-  document.getElementById("datetime24").value = "";
-}
+  var person, reservation;
+
+  function gotData(data) {
+    var id = data.val();
+    var keys = Object.keys(id);
+
+    console.log(keys);
+    for (var propName in keys) {
+      var k = keys[propName];
+      var person = id[k].person;
+      var reservation = id[k].reservation;
+
+      console.log(person);
+      var ul = document.getElementById("lister");
+      var li = document.createElement("li");
+
+      console.log(id);
+      li.appendChild(document.createTextNode(person));
+      li.appendChild(document.createTextNode(reservation));
+
+      // li.class("document");
+      ul.appendChild(li);
+      // li.class('cleanse');
+    }
+  }
+
+  function errData(err) {
+    console.log("Error");
+    console.log(err);
+  }
+
+  //VALIDATION METHODS FOR USER INPUT
+
+  function myValidate(input) {
+    var vz = /[^a-z]/gi;
+    input.value = input.value.replace(vz, "");
+  }
+
+  function complete() {
+    document.getElementById("patron").value = "";
+    document.getElementById("datetime24").value = "";
+  }
+});
