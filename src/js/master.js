a48aa7f87df2670c8a74359ef36fd4ef6372a997
Tom Magin <magintc@mac.com>
2020-07-20 13:13:10 +1000

    js validation

--- src/js/master.js
+++ src/js/master.js
@@ -26,21 +26,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (count > 0) tl.stop();
     else count++;
   };
-  //DATABASE CONFIG AND METHODS
-
-  var config = {
-    apiKey: "AIzaSyDeDgV6YIjgWecHUTwNFP3nu6oPw899gls",
-    authDomain: "master-project-fd122.firebaseapp.com",
-    databaseURL: "https://master-project-fd122.firebaseio.com",
-    projectId: "master-project-fd122",
-    storageBucket: "",
-    messagingSenderId: "462770541013",
-  };
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-  var ref = database.ref("Info"); //info is the node, traversing tree
-  ref.on("value", gotData, errData);
+  
 
   function display() {
     var name = $("#patron").val();
@@ -48,57 +34,50 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var time = $("#datetime24").val();
     console.log(time);
 
-    var newPosts = {
-      person: name,
-      reservation: time,
-    };
-    ref.push(newPosts);
-  }
 
-  ref.on("value", function (snapshot) {
-    console.log(snapshot.key);
-    console.log(snapshot.val());
-  });
-
-  function checker() {
+  function validateInput() {
     let a = document.getElementById("patron").value;
     let b = document.getElementById("datetime24").value;
 
     if (
       a == "" &&
       a == null &&
-      a == undefined &&
+      a == undefined && 
+      a.length < 3 &&
       b == "" &&
       b == null &&
       b == undefined
     ) {
       try {
+        this.style.border = "3px solid red";
         throw "error";
       } catch (err) {
         return err;
       }
-    } else return display();
+    } else {
+      this.style.border = "3px solid green";
+      return display();
   }
 
-  var person, reservation;
+  // var person, reservation;
 
-  function gotData(data) {
-    var id = data.val();
-    var keys = Object.keys(id);
+  // function gotData(data) {
+  //   var id = data.val();
+  //   var keys = Object.keys(id);
 
-    console.log(keys);
-    for (var propName in keys) {
-      var k = keys[propName];
-      var person = id[k].person;
-      var reservation = id[k].reservation;
+  //   console.log(keys);
+  //   for (var propName in keys) {
+  //     var k = keys[propName];
+  //     var person = id[k].person;
+  //     var reservation = id[k].reservation;
 
-      console.log(person);
-      var ul = document.getElementById("lister");
-      var li = document.createElement("li");
+  //     console.log(person);
+  //     var ul = document.getElementById("lister");
+  //     var li = document.createElement("li");
 
-      console.log(id);
-      li.appendChild(document.createTextNode(person));
-      li.appendChild(document.createTextNode(reservation));
+  //     console.log(id);
+  //     li.appendChild(document.createTextNode(person));
+  //     li.appendChild(document.createTextNode(reservation));
 
       // li.class("document");
       ul.appendChild(li);
@@ -106,20 +85,52 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   }
 
-  function errData(err) {
-    console.log("Error");
-    console.log(err);
-  }
+/**Error helper function */
+//   function errData(err) {
+//     console.log("Error");
+//     console.log(err);
+//   }
 
-  //VALIDATION METHODS FOR USER INPUT
+/**Validate name and date inputs */
+//   function myValidate(input) {
+//     var vz = /[^a-z]/gi;
+//     input.value = input.value.replace(vz, "");
+//   }
 
-  function myValidate(input) {
-    var vz = /[^a-z]/gi;
-    input.value = input.value.replace(vz, "");
-  }
-
-  function complete() {
-    document.getElementById("patron").value = "";
-    document.getElementById("datetime24").value = "";
-  }
+/**Clear fields on submit */
+  // function clearFields() {
+  //   document.getElementById("patron").value = "";
+  //   document.getElementById("datetime24").value = "";
+  // }
 });
+
+
+/**Firebase plugged in => comeback to later */
+  //DATABASE CONFIG AND METHODS
+
+  // var config = {
+  //   apiKey: "AIzaSyDeDgV6YIjgWecHUTwNFP3nu6oPw899gls",
+  //   authDomain: "master-project-fd122.firebaseapp.com",
+  //   databaseURL: "https://master-project-fd122.firebaseio.com",
+  //   projectId: "master-project-fd122",
+  //   storageBucket: "",
+  //   messagingSenderId: "462770541013",
+  // };
+  // firebase.initializeApp(config);
+
+  // var database = firebase.database();
+  // var ref = database.ref("Info"); //info is the node, traversing tree
+  // ref.on("value", gotData, errData);
+
+
+    // var newPosts = {
+    //   person: name,
+    //   reservation: time,
+    // };
+    // ref.push(newPosts);
+  }
+
+  // ref.on("value", function (snapshot) {
+  //   console.log(snapshot.key);
+  //   console.log(snapshot.val());
+  // });
