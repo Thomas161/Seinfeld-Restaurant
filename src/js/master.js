cd2e24210a3eded79645177f557773a7b9405b75
Tom Magin <magintc@mac.com>
2020-08-07 18:20:55 +1000

    added some better functionality

--- src/js/master.js
+++ src/js/master.js
@@ -27,72 +27,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     else count++;
   };
 
-  // (function checkNull() {
-  //   if (el && el2) {
-  //     el.addEventListener("onkeyup", myValidateNameAndDate, false);
-  //     el2.addEventListener("onkeyup", myValidateNameAndDate, false);
-  //   } else {
-  //     return false;
-  //   }
-  // });
-
-  function display(e) {
-    var name = $("#patron").val();
-    var time = $("#datetime24").val();
-    if (name && time !== myValidateNameAndDate(e)) {
-      console.log("Perfect");
-    } else {
-      console.log("Error");
-    }
-    e.preventDefault();
-  }
-
-  // function validateInput() {
-  //   if (!myValidateNameAndDate()) {
-  //     try {
-  //       this.style.border = "3px solid red";
-  //       document.getElementsByName("button").disabled = true;
-  //       throw "error";
-  //     } catch (err) {
-  //       return err;
-  //     }
-  //   } else {
-  //     this.style.border = "3px solid green";
-  //     document.getElementsByName("button").diabled = false;
-  //     return display();
-  //   }
-  // }
-
-  // var person, reservation;
-
-  // function gotData(data) {
-  //   var id = data.val();
-  //   var keys = Object.keys(id);
-
-  //   console.log(keys);
-  //   for (var propName in keys) {
-  //     var k = keys[propName];
-  //     var person = id[k].person;
-  //     var reservation = id[k].reservation;
-
-  //     console.log(person);
-  //     var ul = document.getElementById("lister");
-  //     var li = document.createElement("li");
-
-  //     console.log(id);
-  //     li.appendChild(document.createTextNode(person));
-  //     li.appendChild(document.createTextNode(reservation));
-
-  // li.class("document");
-  // ul.appendChild(li);
-  // li.class('cleanse');
-
-  /**Error helper function */
-  //   function errData(err) {
-  //     console.log("Error");
-  //     console.log(err);
-  //   }
-
   /**Validate name and date inputs */
 
   function myValidateNameAndDate(e) {
@@ -103,7 +37,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
       nameResult,
       dateResult,
       emptyName,
-      emptyDate;
+      emptyDate,
+      reservation,
+      node,
+      textNodeName,
+      textNodeDate;
+    reservation = document.getElementById("myList");
+    node = document.createElement("li");
+    textNodeName = $("#patron").val();
+    textNodeDate = $("#datetime24").val();
     inputName = $("#patron").val();
     inputDate = $("#datetime24").val();
     name = new RegExp(/^[a-zA-Z]{3,}$/);
@@ -119,12 +61,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
       inputDate != emptyDate
     ) {
       console.log("true input matches expression");
-      document.getElementById("submitButton").disabled = false;
-
+      reservation.appendChild(document.createTextNode(textNodeName));
+      reservation.appendChild(document.createTextNode(textNodeDate));
       return true;
     } else {
-      console.log("false they dont match");
-      document.getElementById("submitButton").disabled = true;
+      alert("can't submit nothing, or invalid details");
+      // document.getElementById("submitButton").disabled = true;
       return false;
     }
     e.preventDefault();
@@ -144,31 +86,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("datetime24").value = "";
   }
 });
-
-/**Firebase plugged in => comeback to later */
-//DATABASE CONFIG AND METHODS
-
-// var config = {
-//   apiKey: "AIzaSyDeDgV6YIjgWecHUTwNFP3nu6oPw899gls",
-//   authDomain: "master-project-fd122.firebaseapp.com",
-//   databaseURL: "https://master-project-fd122.firebaseio.com",
-//   projectId: "master-project-fd122",
-//   storageBucket: "",
-//   messagingSenderId: "462770541013",
-// };
-// firebase.initializeApp(config);
-
-// var database = firebase.database();
-// var ref = database.ref("Info"); //info is the node, traversing tree
-// ref.on("value", gotData, errData);
-
-// var newPosts = {
-//   person: name,
-//   reservation: time,
-// };
-// ref.push(newPosts);
-//
-
-// ref.on("value", function (snapshot) {
-//   console.log(snapshot.key);
-//   console.log(snapshot.val());
