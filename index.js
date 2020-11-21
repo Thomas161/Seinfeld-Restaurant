791aef31e668e034d68f16c0d47d1cca9afbb2f9
Tom Magin <magintc@mac.com>
2020-11-21 16:03:47 +1100

    Redoing styling

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const http = require("http");
+const fs = require("fs");
+const port = 9000;
+http
+  .createServer((req, res) => {
+    fs.readFile("src/html/Reservations.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(port);
 
-app.get("/", function (req, res) {
-  res.render("Reservation.html");
-});
+// app.get("/", function (req, res) {
+//   res.render("Reservation.html");
+// });
 
-app.listen(`Listening on port :${port}`);
+// app.listen(`Listening on port :${port}`);
